7b1f3ff2dbbc586e220acfdfbfdb2fc3242f324d
chikunmark <chikunmark@gmail.com>
2023-02-18 14:22:19 +0800

    feat: 完成分類篩選功能

--- routes/modules/r-home.js
+++ routes/modules/r-home.js
@@ -4,15 +4,26 @@ const express = require('express')
 const router = express.Router()
 const s_record = require('../../models/s_record') // 載入 record 資料架構
 
+// 顯示首頁
 router.get('/', (req, res) => {
   s_record
     .find()
     .lean()
     .then(records => {
+      // 加總
       const amountArray = records.map(records => records.amount)
       const amount = amountArray.reduce((a, b) => a + b, 0)
       ////////////////////// (上1) 不得不去了解 reduce 了
-      return res.render('index', { records, amount })
+
+      const iconName = {
+        家居物業: 'fa-house',
+        交通出行: 'fa-van-shuttle',
+        休閒娛樂: 'fa-face-grin-beam',
+        餐飲食品: 'fa-utensils',
+        其他: 'fa-pen',
+      }
+
+      return res.render('index', { records, amount, iconName: iconName[records.categoryName] })
     })
     .catch(err => console.error(err))
 })
--- routes/modules/r-home.sortAndFilter.js
+++ routes/modules/r-home.sortAndFilter.js
@@ -1,6 +1,27 @@
 // 這是 route 設定
 
 const express = require('express')
+const s_record = require('../../models/s_record')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+  let filter = req.query.filter
+
+  // 先把所有 records 找出，再篩選
+  return s_record
+    .find()
+    .lean()
+    .then(records => {
+      // 若 record 的 categoryName 與 filter 條件吻合，就篩出來 (若篩選值是 '' 或 undefine，全都 true)
+      const filterRecords = records.filter(record => record.categoryName.includes(filter))
+
+      // 加總，有多處用到，可能可包裝成 Fn.
+      const amountArray = filterRecords.map(records => records.amount)
+      const amount = amountArray.reduce((a, b) => a + b, 0)
+      ////////////////////// (上1) 不得不去了解 reduce 了
+
+      return res.render('index', { records: filterRecords, amount, filter })
+    })
+})
 
 module.exports = router
