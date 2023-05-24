3d2a33c89c73b0cf6de5daa41f4620feb168d684
chikunmark <chikunmark@gmail.com>
2023-05-24 15:21:46 +0800

    fix: store issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 載入 addon
 const express = require('express')
 const session = require('express-session')
+const MongoStore  = require('connect-mongo') // store 有問題，自己加看看
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const helper = require('handlebars-helpers')() // 用了額外 add-on，之後想有無更簡單解法
@@ -23,12 +24,9 @@ app.use(express.static('public'))
 app.use(
   session({
     secret: process.env.SESSION_SECRET, // 這參數是 session 用來驗證 session id 的字串，由我們設定，可改成隨機的一個字串
-    cookie:{  // 不確定用途
-      secure: true,
-      maxAge:60000
-    },
     resave: false, // 若為 true，每次有新的 request，就會把 session 更新到 session store，然後蓋過去
     saveUninitialized: true, // 若為 true，強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }) // store 有問題，自己加看看
   })
 )
 
